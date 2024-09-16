8cf2601bbda66ef73c8d46cffc74eea0c4efba29
Alex <playalexei@yandex.ru>
2024-09-16 20:00:37 +0700

    v1, для парсинга proxy используется Selenium

---
+++ main.py
@@ -0,0 +1,95 @@
+import datetime
+import json
+import os
+import random
+import time
+import traceback
+
+import openpyxl
+import pytz
+import requests
+
+from proxy import getProxies, findProxy, checkProxy
+
+xlsx_file = 'rezult.xlsx'
+mouser_api_key = 'a6adbd72-cc89-4454-a34c-dd0f75364d52'  # Нашёл в интернете
+
+
+def parse_stock(name, proxy):
+    proxies = {
+        "https": proxy,
+    }
+    body = json.dumps({
+        "SearchByKeywordRequest": {
+            "keyword": name,
+            "records": 10,
+            "startingRecord": 0,
+            "searchOptions": "string",
+            "searchWithYourSignUpLanguage": "string"
+        }
+    })
+    headers = {"Content-Type": "application/json"}
+    url = f'https://api.mouser.com/api/v1/search/keyword?apiKey={mouser_api_key}'
+    r = requests.post(url, data=body, proxies=proxies, headers=headers)
+    if r.status_code == 200:
+        data = json.loads(r.text)
+        for part in data["SearchResults"]["Parts"]:
+            if part["ManufacturerPartNumber"] == name:
+                if "In Stock" in part["Availability"]:
+                    return int(part["Availability"].split()[0])
+                else:
+                    return 0
+        else:
+            return -1  # Обозначает не нахождение позиции
+
+
+def main():
+    if not os.path.exists(xlsx_file):
+        raise Exception("Файл «{xlsx_file}» не найден.")
+
+    proxy = None
+    if not os.path.exists('proxy.txt'):
+        print("Поиск proxy...")
+        proxies = getProxies()
+        working_proxy = findProxy(proxies)
+        with open('proxy.txt', 'w') as f:
+            f.write(working_proxy)
+        proxy = working_proxy
+    else:
+        with open('proxy.txt', 'r') as f:
+            working_proxy = f.readline().strip()
+        print("Проверка сохранённого proxy...")
+        if checkProxy(working_proxy) is False:
+            print("Поиск нового proxy...")
+            proxies = getProxies()
+            working_proxy = findProxy(proxies)
+            with open('proxy.txt', 'w') as f:
+                f.write(working_proxy)
+        proxy = working_proxy
+    if proxy is None:
+        print("PROXY не найден! (✘ᆺ✘)")
+    else:
+        print(f"PROXY успешно найден! (>⩊<): {working_proxy}")
+
+    wookbook = openpyxl.load_workbook(xlsx_file)
+    worksheet = wookbook.active
+    worksheet.cell(row=1, column=4).value = datetime.datetime.now(pytz.timezone('Europe/Moscow')).strftime('%d-%m-%Y_ '
+                                                                                                           '%H:%M:%S')
+    for row_i in range(2, worksheet.max_row + 1):
+        name = worksheet.cell(row=row_i, column=2).value
+        stock = parse_stock(name, proxy)
+        if stock == -1:
+            worksheet.cell(row=row_i, column=4).value = 'Ошибка'
+        else:
+            worksheet.cell(row=row_i, column=4).value = stock
+        time.sleep(random.randint(1, 10) // 10)
+    wookbook.save(xlsx_file)
+
+
+if __name__ == '__main__':
+    try:
+        main()
+    except Exception:
+        traceback.print_exc()
+        print("\nПрограмма завершит свою работу через 120 секунд.")
+        time.sleep(120)
--- proxy.py
+++ proxy.py
@@ -1,1 +1,57 @@
-from fp.fp import FreeProxy
+import time
+import traceback
+
+import requests
+import undetected_chromedriver as uc
+from bs4 import BeautifulSoup
+from selenium.webdriver.common.by import By
+
+
+def checkProxy(prx):
+    try:
+        s = requests.Session()
+        proxies = {
+            'https': f"{prx}",
+        }
+        s.proxies.update(proxies)
+        s.headers.update({"User-Agent": "Opera/9.80 (X11; Linux x86_64; U; de) Presto/2.2.15 Version/10.00"})
+        r = s.get(f'https://api.mouser.com/', timeout=8)
+        if '<title>mouser.com</title>' in r.text:
+            return prx
+    except Exception as e:
+        # traceback.print_exc()
+        return False
+
+
+def getProxies():
+    browser = uc.Chrome(headless=True)
+    browser.get('http://free-proxy.cz/ru/')
+    time.sleep(2)
+    browser.find_element(By.XPATH, '//*[@id="frmsearchFilter-protocol-2"]').click()
+    browser.find_element(By.XPATH, '//*[@id="frmsearchFilter-send"]').click()
+    time.sleep(1.5)
+    html = browser.page_source
+    soup = BeautifulSoup(html, 'lxml')
+    table = soup.find_all('tbody')[1].find_all('tr')
+    proxies = []
+    for row in table:
+        try:
+            proxy = ':'.join([row.find_all('td')[0].text, row.find_all('td')[1].text])
+            proxies.append(proxy)
+        except IndexError:
+            continue
+    return proxies
+
+
+def findProxy(prxs):
+    for pr in prxs:
+        res = checkProxy(pr)
+        if res is not False:
+            return res
+    return None
+
+
+if __name__ == '__main__':
+    proxies2 = getProxies()
+    proxy = findProxy(proxies2)
+    working_proxy = findProxy(proxies2)
